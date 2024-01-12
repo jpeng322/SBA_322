235ff47c0c27509b5e2e27e41b567a17be034273
Jacky <jackypengsmail@gmail.com>
2024-01-11 23:55:24 -0500

    Initialized vue app

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>This is the header.</h1>
+  </header>
+</template>
+
+<script>
+// export default {
+//   name: Header,
+  
+// };
+</script>
+
+<style scoped>
+header {
+  border: 1px solid red;
+}
+</style>
